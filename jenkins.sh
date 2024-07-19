7b12bb9ec108912191aeca24d62b441835188b73
sravani586 <63241196+sravani586@users.noreply.github.com>
2024-07-18 22:53:07 -0400

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+
 env=$1
 tag=$2
 
-echo $env
+echo "Environment: $env"
+echo "Tag: $tag"
 
-if ["$env" == "QA"]; then 
-   echo "performing $env $tag"
-else 
-   echo "select the correct env"
+if [ "$env" == "QA" ]; then
+    echo "Performing $env operations with tag $tag"
+else
+    echo "Select the correct environment"
 fi
